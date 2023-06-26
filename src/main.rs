5528cd3aea9c5bcbb12891fac3df273d6a4e7afe
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-26 18:49:45 +0530

    chip info

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
+use std::process::Command;
 
-use std::fs::File;
-use std::io::{self, Read};
+fn main() {
+    let output = Command::new("/MECHA_TEST/optiga_trust_m/trustm_chipinfo")
+        .output()
+        .expect("Failed to execute the command");
 
-fn main() -> io::Result<()> {
-    // Open the file with a relative path
-    let mut file = File::open("/MECHA_TEST/optiga_trust_m/trustm_chipinfo")?;
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let lines: Vec<&str> = stdout.split('\n').collect();
 
-    // Read the contents of the file into a buffer
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
-
-    // Process the data in the buffer
-    // Convert the bytes to a string
-    let output = String::from_utf8_lossy(&buffer);
-
-    // Print the captured output
-    println!("{}", output);
-
-    Ok(())
+        for line in lines {
+            if line.contains(":") {
+                let parts: Vec<&str> = line.split(":").collect();
+                let key = parts[0].trim();
+                let value = parts[1].trim();
+                println!("{}: {}", key, value);
+            }
+        }
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        println!("Command executed with error: {}", stderr);
+    }
 }
+
